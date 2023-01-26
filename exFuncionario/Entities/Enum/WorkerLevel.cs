278ebb4e69ec7c46432df25f276bbc060166579a
Fernando Balieiro <fernando.balieiro0580@hotmail.com>
2023-01-26 14:22:21 -0300

    Add project files.

---
+++ exFuncionario/Entities/Enum/WorkerLevel.cs
@@ -0,0 +1,10 @@
+﻿namespace exFuncionario.Entities.Enum
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+
+}
---
+++ exFuncionario/Entities/HourContract.cs
@@ -0,0 +1,21 @@
+﻿namespace exFuncionario.Entities
+{
+    class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int Hour { get; set; }
+
+        public HourContract(DateTime date, double valuePerHour, int hour)
+        {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            Hour = hour;
+        }
+
+        public double TotalValue()
+        {
+            return Hour * ValuePerHour;
+        }
+    }
+}
